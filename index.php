b7e29b34da311524324f2c4ca21ad08df2239316
Abhishek Konar <48175493+abhishek-konar@users.noreply.github.com>
2023-05-17 13:43:25 +0530

    style

--- index.php
+++ index.php
@@ -12,14 +12,18 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark static-top">
-		<div class="container">
-			<a class="navbar-brand" href="/">Stackle</a>
+<nav class="navbar navbar-expand-lg navbar-dark static-top">
+		<div class="container p-0">
+			<a class="navbar-brand" href="index.php">Stackle</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
-			<div class="collapse navbar-collapse" id="navbarResponsive">
-				<ul class="navbar-nav ml-auto">
+      
+
+  <div class="collapse navbar-collapse" id="navbarResponsive">
+    <ul class="navbar-nav mr-auto">
+      
+      
           <li class="nav-item">
 						<a class="nav-link" href="./login.php">Login</a>
           </li>
@@ -27,19 +31,15 @@
 						<a class="nav-link" href="./signup.php">Sign up</a>
           </li>
           <li class="nav-item">
-						<a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="#"><img class="cart" src="https://img.icons8.com/fluency/48/shopping-cart.png" alt="shopping-cart"/><sup>1</sup></a>
           </li>
-				</ul>
-			</div>
-		</div>
-	</nav>
-  
-  <div class="content">
-    <p class='big'>Hey, Hi There !</p>
-</div>
-
-
-
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
 </body>
 </html>
 
