3a28911fe92577296c0458548da6d41083f31269
Abhishek Konar <48175493+abhishek-konar@users.noreply.github.com>
2023-05-18 16:16:07 +0530

    a

--- server.php
+++ server.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 
 <head>
     <meta charset="UTF-8">
@@ -13,43 +12,52 @@
 </head>
 <body>
 
+<?php
+error_reporting(0);
+session_start();
+$email=$_SESSION['em'];
+$pass=$_SESSION['pa'];
+$c=mysqli_connect('localhost', 'root', '', 'abhishek');
+$q="SELECT * FROM user WHERE email='$email'AND pasword='$pass'";
+$res=mysqli_query($c, $q);
+if(mysqli_num_rows($res)==0)
+{
+    session_start();
+    session_destroy();
+    header('location:login.php');
+}
+while($row=mysqli_fetch_assoc($res)){
+    $n=$row["name"];
+}
+?>
 
-
-<nav class="navbar navbar-expand-lg navbar-dark static-top">
-		<div class="container">
-			<a class="navbar-brand" to="/">Stackle</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-      
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Stackle</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Other</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Cart</a>
+        <a class="nav-link" href="#">Logout</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
       </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+             <?php echo "Hellow, ". $n; ?>
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Your Account</a>
+          <a class="dropdown-item" href="logout.php">Logout</a>
           <a class="dropdown-item" href="#">Something else here</a>
         </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="logout.php">Lgout</a>
-      </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
 </nav>
 
