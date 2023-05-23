42e9a7e2f6a1914d9ca83f4b90c1538d71b48146
abhi <abhishek.konar60@gmail.com>
2023-05-23 22:02:57 +0530

    a

---
+++ add_address.php
@@ -0,0 +1,73 @@
+<?php include ('./server.php'); ?>
+
+<!--Address Form-->
+<form  name="insert" method="post" action="" onsubmit="return myfunction()" >    
+    <div class="wrap">
+          <p class="p1">Add a new address</p>
+          <input class="textbox"  type="text" id="name" name="name" placeholder="Full name (First and Last name)">
+          <input class="textbox" type="text" id="mobilenumbe" name="phone" placeholder="Mobile number" maxlength="10">
+          <input class="textbox"  type="pin" id="pin" name="pin" placeholder="Pin" maxlength="9">
+          <input class="textbox"  type="text" id="area" name="area" placeholder="Area, Street, Sector, Village">
+          <input class="textbox"  type="text" id="city" name="city" placeholder="Town/City">
+          <input class="textbox"  type="text" name="land" placeholder="Landmark (E.g. near Stackle House)">
+            <div class="t">
+                <select id="state" name="state" Class="s">
+                    <option value="">Select Yours State Name</option>
+                    <option value="Andhra Pradesh">Andhra Pradesh</option>
+                    <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+                    <option value="Assam">Assam</option>
+                    <option value="Bihar">Bihar</option>
+                    <option value="Chhattisgarh">Chhattisgarh</option>
+                    <option value="Goa">Goa</option>
+                    <option value="Gujarat">Gujarat</option>
+                    <option value="Haryana">Haryana</option>
+                    <option value="Himachal Pradesh">Himachal Pradesh</option>
+                    <option value="Jharkhand">Jharkhand</option>
+                    <option value="Karnataka">Karnataka</option>
+                    <option value="Kerala">Kerala</option>
+                    <option value="Madhya Pradesh">Madhya Pradesh</option>
+                    <option value="Maharashtra">Maharashtra</option>
+                    <option value="Manipur">Manipur</option>
+                    <option value="Meghalaya">Meghalaya</option>
+                    <option value="Mizoram">Mizoram</option>
+                    <option value="Nagaland">Nagaland</option>
+                    <option value="Odisha">Odisha</option>
+                    <option value="Punjab">Punjab</option>
+                    <option value="Rajasthan">Rajasthan</option>
+                    <option value="Sikkim">Sikkim</option>
+                    <option value="West Bengal">West Bengal</option>
+                </select>
+        </div>
+        <input class="textbox"  type="text" id="Country" name="Country" placeholder="Country/Region">
+        <p style="color:red" id="myalert"></p>
+        <input type="submit" value="Add address" class="submit" name="insert">
+    </div>
+  </form>
+  
+
+
+<!--Insert Address-->
+<?php 
+
+    error_reporting(0);
+    session_start();
+    $email=$_SESSION['em'];
+
+    if (isset ($_POST['insert'])){
+      $con=mysqli_connect('localhost', 'root', '', 'abhishek');
+    }
+
+    if($con){
+        $name= $_POST['name'];
+        $phone=$_POST['phone'];
+        $pin=$_POST['pin'];
+        $area=$_POST['area'];
+        $land=$_POST['land'];
+        $state=$_POST['state'];
+        $city=$_POST['city'];
+        $Country=$_POST['Country'];
+        $sql="INSERT INTO address (email, Country, name, phone, pin, area, land, city, state) VALUE ('$email', '$Country', '$name', '$phone', '$pin', '$area', '$land', '$city', '$state')";
+        $run=mysqli_query($con, $sql);
+    }
+    
+  ?>
--- address.php
+++ address.php
@@ -1,73 +1,91 @@
 <?php include ('./server.php'); ?>
 
-<!--Address Form-->
-<form  name="insert" method="post" action="" onsubmit="return myfunction()" >    
-    <div class="wrap">
-          <p class="p1">Add a new address</p>
-          <input class="textbox"  type="text" id="name" name="name" placeholder="Full name (First and Last name)">
-          <input class="textbox" type="text" id="mobilenumbe" name="phone" placeholder="Mobile number" maxlength="10">
-          <input class="textbox"  type="pin" id="pin" name="pin" placeholder="Pin" maxlength="9">
-          <input class="textbox"  type="text" id="area" name="area" placeholder="Area, Street, Sector, Village">
-          <input class="textbox"  type="text" id="city" name="city" placeholder="Town/City">
-          <input class="textbox"  type="text" name="land" placeholder="Landmark (E.g. near Stackle House)">
-            <div class="t">
-                <select id="state" name="state" Class="s">
-                    <option value="">Select Yours State Name</option>
-                    <option value="Andhra Pradesh">Andhra Pradesh</option>
-                    <option value="Arunachal Pradesh">Arunachal Pradesh</option>
-                    <option value="Assam">Assam</option>
-                    <option value="Bihar">Bihar</option>
-                    <option value="Chhattisgarh">Chhattisgarh</option>
-                    <option value="Goa">Goa</option>
-                    <option value="Gujarat">Gujarat</option>
-                    <option value="Haryana">Haryana</option>
-                    <option value="Himachal Pradesh">Himachal Pradesh</option>
-                    <option value="Jharkhand">Jharkhand</option>
-                    <option value="Karnataka">Karnataka</option>
-                    <option value="Kerala">Kerala</option>
-                    <option value="Madhya Pradesh">Madhya Pradesh</option>
-                    <option value="Maharashtra">Maharashtra</option>
-                    <option value="Manipur">Manipur</option>
-                    <option value="Meghalaya">Meghalaya</option>
-                    <option value="Mizoram">Mizoram</option>
-                    <option value="Nagaland">Nagaland</option>
-                    <option value="Odisha">Odisha</option>
-                    <option value="Punjab">Punjab</option>
-                    <option value="Rajasthan">Rajasthan</option>
-                    <option value="Sikkim">Sikkim</option>
-                    <option value="West Bengal">West Bengal</option>
-                </select>
+
+<!--All address Details-->
+<?php
+error_reporting(0);
+session_start();
+$email=$_SESSION['em'];
+$c=mysqli_connect('localhost', 'root', '', 'abhishek');
+$q="SELECT * FROM address WHERE email='$email'";
+$res=mysqli_query($c, $q);
+
+while($row=mysqli_fetch_assoc($res)){
+    $name=$row['name'];
+    $phone=$row['phone'];
+    $pin=$row['pin'];
+    $area=$row['area'];
+    $city=$row['city'];
+    $land=$row['land'];
+    $state=$row['state'];
+    $phone=$row['phone'];
+
+echo "<div class='wrapper'>
+        <div class='cards_wrap'>".
+            "<div class='card_item'>".
+                "<div class='card_inner'>".
+                    "<div class='medium'>" .$name. "</div>
+                    <div class='film'>".$area. ", </div>
+                    <div class='film'>".$city. ", ". $state. " " .$pin. "</div>
+                    <div class='film'> Land mark: ".$land. "</div>
+                    <div class='film'>Phone number: ". $phone. "</div>
+                </div>
+            </div>
         </div>
-        <input class="textbox"  type="text" id="Country" name="Country" placeholder="Country/Region">
-        <p style="color:red" id="myalert"></p>
-        <input type="submit" value="Add address" class="submit" name="insert">
-    </div>
-  </form>
-  
+        
+    </div>";
 
 
-<!--Insert Address-->
-<?php 
 
-    error_reporting(0);
-    session_start();
-    $email=$_SESSION['em'];
+}
+?>
 
-    if (isset ($_POST['insert'])){
-      $con=mysqli_connect('localhost', 'root', '', 'abhishek');
-    }
 
-    if($con){
-        $name= $_POST['name'];
-        $phone=$_POST['phone'];
-        $pin=$_POST['pin'];
-        $area=$_POST['area'];
-        $land=$_POST['land'];
-        $state=$_POST['state'];
-        $city=$_POST['city'];
-        $Country=$_POST['Country'];
-        $sql="INSERT INTO address (email, Country, name, phone, pin, area, land, city, state) VALUE ('$email', '$Country', '$name', '$phone', '$pin', '$area', '$land', '$city', '$state')";
-        $run=mysqli_query($con, $sql);
-    }
-    
-  ?>
+
+
+<div class="wrapper">
+
+	
+<!--Tab 1-->
+	<div class="cards_wrap">
+		<div class="card_item">
+			<div class="card_inner">
+                <div class="medium"><?php echo $name; ?></div>
+                <div class="film"><?php echo $area. ","; ?></div>
+                <div class="film"><?php echo $city. ", ". $state. " " .$pin; ?></div>
+				<div class="film"><?php echo "Phone number: ". $phone; ?></div>
+                <div class="film"><?php echo "Land mark: ".$land; ?></div>
+			</div>
+		</div>
+
+<!--Tab 2-->
+		<div class="card_item">
+			<a href="add_address.php" style="text-decoration: none;">
+			<div class="card_inner">
+				<img src="./assets/address.svg">
+                <div class="medium">Add your Address</div>
+                <div class="film">Add your Address </div>
+			</div>
+			</a>
+		</div>
+
+		
+<!--Tab 3-->
+		<div class="card_item">
+			<div class="card_inner">
+				<img  src="./assets/demo.svg">
+                 <div class="medium">Contact</div>
+                 <div class="film">Edit and Add yours Phone number </div>
+			</div>
+		</div>
+		
+<!--Tab 3-->
+		<div class="card_item">
+			<div class="card_inner">
+				<img src="./assets/contact_us.svg">
+                 <div class="medium">Contact Us</div>
+                 <div class="film">Edit, Add and Delete your Address</div>
+			</div>
+		</div>
+	</div>
+</div> 
