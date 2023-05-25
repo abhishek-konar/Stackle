bd0a8438223face8e182920a7360534cdef75ee2
abhi <abhishek.konar60@gmail.com>
2023-05-25 16:43:02 +0530

    aaa

--- address.php
+++ address.php
@@ -1,22 +1,26 @@
 <?php include ('./server.php'); ?>
 
-<div class="wrapper">
-    <div class="header">Your Address</div>
-    <div class="cards_wrap">
-            <div class="card_item">
-             <a href="add_address.php" style="text-decoration: none;">
-                <div class="card_inner">
-                    <div class="medium"></div>
-                    <div class="film"></div>
-                    <div class="film"></div>
-                    <div class="film"> Land mark:</div>
-                    <div class="film">Phone number</div>
-                </div>
-            </a>
-            </div>
 
+<div class="pt-5 pb-5">
+    <div class="container">
+      <div class="row">
+        <div class="section-head col-sm-12">
+          <h4><span>Your Addresses </h4>
+        </div>
+
+		<!--Add New Addres-->
+        <div class="col-lg-4 col-sm-6">
+		<a href="add_address.php" style="text-decoration: none;">
+          <div class="item"> <span class="icon feature_box_col_one"></span></i></span>
+            <h6>Modern Design</h6>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor Aenean massa.</p>
+          </div>
+		</a>
+        </div>
+
+		
 <!--All address Details-->
-        <?php
+<?php
         error_reporting(0);
         session_start();
         $email=$_SESSION['em'];
@@ -33,19 +37,22 @@
         $land=$row['land'];
         $state=$row['state'];
         $phone=$row['phone'];
-        echo "<div class='card_item'>".
-                "<div class='card_inner'>".
-                    "<div class='medium'>" .$name. "</div>
-                    <div class='film'>".$area. ", </div>
-                    <div class='film'>".$city. ", ". $state. " " .$pin. "</div>
-                    <div class='film'> Land mark: ".$land. "</div>
-                    <div class='film'>Phone number: ". $phone. "</div>
-                    <input type='submit' value='Edit'>
-                    <input type='submit' value='Delete'>
-                </div>
-            </div>";
+        echo "<div class='col-lg-4 col-sm-6'>".
+        "<div class='item'>". "<span class='icon feature_box_col_one'></span></i></span>".
+          "<h6>". $name. "</h6>".
+          "<p>".$area. ", </p>".
+          "<p>".$city. ", ". $state. " " .$pin."</p>".
+          "<p> Landmar: ".$land."</p>".
+          "<p>".$phone."</p>".
+        "</div>
+      </div>";       
 }
 ?>
-
+      </div>
     </div>
 </div>
+
+
+
+
+
--- myaccount.php
+++ myaccount.php
@@ -1,46 +1,47 @@
 <?php include ('./server.php'); ?>
 
-<div class="wrapper">
 
-	<div class="header">Your Account</div>
-<!--Tab 1-->
-	<div class="cards_wrap">
-		<div class="card_item">
-			<div class="card_inner">
-			<img src="./assets/login.svg">
-            <div class="medium">Login & Security</div>
-				<div class="film">Edit login, name and Pasdword</div>
-			</div>
-		</div>
 
-<!--Tab 2-->
-		<div class="card_item">
+<body>
+  <div class="pt-5 pb-5">
+    <div class="container">
+      <div class="row">
+        <div class="section-head col-sm-12">
+          <h4><span>Why Choose</span> Me?</h4>
+        </div>
+
+		<!--Tab 1-->
+        <div class="col-lg-4 col-sm-6">
+			<a href="add_address.php" style="text-decoration: none;">
+          <div class="item"> <span class="icon feature_box_col_one"></span></i></span>
+            <h6>Modern Design</h6>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor Aenean massa.</p>
+          </div>
+		  </a>
+        </div>
+
+		
+       <!--Tab 2-->
+	   <div class="col-lg-4 col-sm-6">
 			<a href="address.php" style="text-decoration: none;">
-			<div class="card_inner">
-				<img src="./assets/address.svg">
-                <div class="medium">Your Address</div>
-                <div class="film">Edit, Add and Delete your Address </div>
-			</div>
-			</a>
-		</div>
+          <div class="item"> <span class="icon feature_box_col_one"></span></i></span>
+            <h6>Address</h6>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor Aenean massa.</p>
+          </div>
+		  </a>
+        </div>
 
-		
-<!--Tab 3-->
-		<div class="card_item">
-			<div class="card_inner">
-				<img  src="./assets/demo.svg">
-                 <div class="medium">Contact</div>
-                 <div class="film">Edit and Add yours Phone number </div>
-			</div>
-		</div>
-		
-<!--Tab 3-->
-		<div class="card_item">
-			<div class="card_inner">
-				<img src="./assets/contact_us.svg">
-                 <div class="medium">Contact Us</div>
-                 <div class="film">Edit, Add and Delete your Address</div>
-			</div>
-		</div>
-	</div>
-</div> 
+		 <!--Tab 3-->
+		 <div class="col-lg-4 col-sm-6">
+          <div class="item"> <span class="icon feature_box_col_six"><i class="fa fa-camera"></i></span>
+            <h6>Retina Ready</h6>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor Aenean massa.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+</body>
+
+
+
